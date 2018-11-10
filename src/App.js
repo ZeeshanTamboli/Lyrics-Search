3874583108b162641b9c15846ca8866007025b71
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2018-11-10 14:59:30 +0530

    Setup deployment for gh pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
