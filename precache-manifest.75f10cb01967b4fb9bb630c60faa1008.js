fea0919d4855052fbc7029bcc7411b038d66f369
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2018-11-10 15:01:38 +0530

    Updates

---
+++ precache-manifest.75f10cb01967b4fb9bb630c60faa1008.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cf18c17d6f725bb0121",
+    "url": "/Lyrics-Search/static/js/runtime~main.0cf18c17.js"
+  },
+  {
+    "revision": "17e13537dda0866b60e1",
+    "url": "/Lyrics-Search/static/js/main.17e13537.chunk.js"
+  },
+  {
+    "revision": "730c57af6f0842b420af",
+    "url": "/Lyrics-Search/static/js/1.730c57af.chunk.js"
+  },
+  {
+    "revision": "17e13537dda0866b60e1",
+    "url": "/Lyrics-Search/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "f0fa754e71a88ad9a785e136c3296b05",
+    "url": "/Lyrics-Search/index.html"
+  }
+];
